ab07238944f1a69cde447ab87f781e19e74bfcd6
JAlexanderMatailo <52136249+JAlexanderMatailo@users.noreply.github.com>
2023-09-12 11:36:44 -0500

    configuracion de entorno, instalcaion de paquetes nuggets para desarrollo

--- CargosEnDepartamentos/Program.cs
+++ CargosEnDepartamentos/Program.cs
@@ -2,10 +2,38 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//builder.Services.AddScoped<IPersona, PersonaService>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+#region Conexion Base de Datos
+
+//builder.Services.AddDbContext<SaaccazhapugroContext>(options =>
+//{
+//    options.UseSqlServer(builder.Configuration.GetSection("AppSettings").GetSection("DefaultConnection").Value);
+
+//});
+
+var appSettingsSection = builder.Configuration.GetSection("AppSettings");
+
+#endregion
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "API",
+                      builder =>
+                      {
+                          builder.WithHeaders("*");
+                          builder.WithOrigins("*");
+                          builder.WithMethods("*");
+
+                      });
+});
+
 
 var app = builder.Build();
 
@@ -19,6 +47,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("API");
 
 app.MapControllers();
 
